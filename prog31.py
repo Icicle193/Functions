0e0f33e3ba0a0744f599a34cc57dd3bc9f5c498a
Icicle193 <nevaankumar2015@gmail.com>
2025-11-07 19:35:58 -0500

    Add files via upload

---
+++ prog31.py
@@ -0,0 +1,12 @@
+def well_wishes():
+    print("hello")
+    print("how are you?")
+
+well_wishes()
+
+def adding(a, b):
+    c = a + b
+    return c
+
+result = adding(22000000, 22000000)
+print(result)
